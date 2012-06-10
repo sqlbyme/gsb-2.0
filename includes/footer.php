bca1c35f0a0f1967460707f3849c6c6078d03900
Mike Edwards <mike@songbirdnest.com>
2012-06-10 13:47:07 -0700

    Updated some footer links to current pages.

--- includes/footer.php
+++ includes/footer.php
@@ -6,11 +6,11 @@
         <li><a href="#">Partners</a></li>
         <li><a href="#">Products</a></li>
         <li><a href="#">Support</a></li>
-        <li><a href="overview.php">About</a></li>
+        <li><a href="overview.php">About Us</a></li>
         <li><a href="#">Jobs</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Privacy</a></li>
-        <li><a href="#">Terms of Use</a></li>
+        <li><a href="eula.php">License</a></li>
+        <li><a href="privacy.php">Privacy</a></li>
+        <li><a href="terms.php">Terms of Use</a></li>
       </ul> 
     <a class="facebook" href="http://www.facebook.com/Songbird?ref=search&sid=648331437.2719007091..1" target="_blank"></a><a class="twitter" href="https://twitter.com/#!/songbird" target="_blank"></a>
   </div><!--footer-content -->
