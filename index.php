8aede1b22a9bb95ec7caa5920ed43014c8260e53
Mike Edwards <mike@songbirdnest.com>
2012-06-10 11:44:44 -0700

    Moved the footer content from index.php to its own file includes/footer.php.  Corrected style.css to reflect new footer layout.

--- index.php
+++ index.php
@@ -99,24 +99,7 @@ jQuery(function(){
 
 <div style="clear:both;"></div>
 
-<div id="footer">
-  <div id="footer-content">
-    <p>2012 &copy; SONGBIRD All Rights Reserved &nbsp;&nbsp; | &nbsp;</p>
-      <ul>
-        <li><a href="#">Help</a></li>
-        <li><a href="#">Partners</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">Support</a></li>
-        <li><a href="overview.php">About</a></li>
-        <li><a href="#">Jobs</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Privacy</a></li>
-        <li><a href="#">Terms of Use</a></li>
-      </ul> 
-    <a class="facebook" href="http://www.facebook.com/Songbird?ref=search&sid=648331437.2719007091..1" target="_blank"></a><a class="twitter" href="https://twitter.com/#!/songbird" target="_blank"></a>
-  </div><!--footer-content -->
-  <div class="clearfix"></div>
-</div><!--footer -->
+<?php include('includes/footer.php'); ?>
 
 <div class="clearfix"></div>
 
