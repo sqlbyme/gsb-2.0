a39518e0c41112ac87524ae5402556b359565c92
Mike Edwards <mike@songbirdnest.com>
2012-06-11 10:47:37 -0700

    Updated footer to include GA events tracking.

--- includes/footer.php
+++ includes/footer.php
@@ -2,17 +2,17 @@
   <div id="footer-content">
     <p>2012 &copy; All Rights Reserved &nbsp;&nbsp; | &nbsp;</p>
       <ul>
-        <li><a href="../help/">Help</a></li>
-        <li><a href="../partners/">Partners</a></li>
-        <li><a href="/products/">Products</a></li>
-        <li><a href="http://getsatisfaction.com/songbird">Support</a></li>
-        <li><a href="../about/overview.php">About Us</a></li>
-        <li><a href="#">Jobs</a></li>
-        <li><a href="../about/eula.php">License</a></li>
-        <li><a href="../about/privacy.php">Privacy</a></li>
-        <li><a href="../about/terms.php">Terms of Use</a></li>
+        <li><a href="../help/" onClick="recordOutboundLink(this, 'Help Clicked', 'Link Location: Page Footer');">Help</a></li>
+        <li><a href="../partners/" onClick="recordOutboundLink(this, 'Partners Clicked', 'Link Location: Page Footer');">Partners</a></li>
+        <li><a href="/products/" onClick="recordOutboundLink(this, 'Products Clicked', 'Link Location: Page Footer');">Products</a></li>
+        <li><a href="http://getsatisfaction.com/songbird" onClick="recordOutboundLink(this, 'Support Clicked', 'Link Location: Page Footer'); return false;">Support</a></li>
+        <li><a href="../about/overview.php" onClick="recordOutboundLink(this, 'About Clicked', 'Link Location: Page Footer');">About Us</a></li>
+        <li><a href="#" onClick="recordOutboundLink(this, 'Jobs Clicked', 'Link Location: Page Footer');">Jobs</a></li>
+        <li><a href="../about/eula.php" onClick="recordOutboundLink(this, 'License Clicked', 'Link Location: Page Footer');">License</a></li>
+        <li><a href="../about/privacy.php" onClick="recordOutboundLink(this, 'Privacy Clicked', 'Link Location: Page Footer');">Privacy</a></li>
+        <li><a href="../about/terms.php" onClick="recordOutboundLink(this, 'Terms Clicked', 'Link Location: Page Footer');">Terms of Use</a></li>
       </ul> 
-    <a class="facebook" href="http://www.facebook.com/Songbird?ref=search&sid=648331437.2719007091..1" target="_blank"></a><a class="twitter" href="https://twitter.com/#!/songbird" target="_blank"></a>
+    <a class="facebook" href="http://www.facebook.com/Songbird" target="_blank" onClick="recordOutboundLink(this, 'Facebook Clicked', 'Link Location: Page Footer'); return false;"></a><a class="twitter" href="https://twitter.com/#!/songbird" target="_blank" onClick="recordOutboundLink(this, 'Twitter Clicked', 'Link Location: Page Footer');"></a>
   </div><!--footer-content -->
   <div class="clearfix"></div>
 </div><!--footer -->
