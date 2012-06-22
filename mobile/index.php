8d2bcdedaaa10738f8649da2e1322b546de1fbf7
Mike Edwards <mike@songbirdnest.com>
2012-06-22 13:16:40 -0700

    Updated email submit buttons and layout per: https://www.pivotaltracker.com/story/show/31090291 .

--- mobile/index.php
+++ mobile/index.php
@@ -22,6 +22,7 @@
   function captureEmail () {
     var emailElement = document.getElementById("email_addr");
     var optInChecked = document.getElementById("email_optin");
+    var emailButton = document.getElementById("email_submit_btn");
     if ( emailElement.value.length > 0 && optInChecked.checked == true && emailElement.disabled != true) {
       $.get(
 	    "../lib/mailchimp/subscribe.php" ,
@@ -29,11 +30,13 @@
 	  );
 	  emailElement.value = "Thank You!";
 	  emailElement.disabled = true;
+	  emailButton.style.display = "none";
+	  emailElement.style.border = "0px solid #999";
     };
    };
   function captureiPhoneEmail () {
     var emailElement = document.getElementById("iPhone_email_addr");
-    var emailSubmitBtn = document.getElementById("email_submit_btn");
+    var emailSubmitBtn = document.getElementById("iPhone_email_submit_btn");
     if ( emailElement.value.length > 0 ) {
       $.get(
 	    "../lib/mailchimp/subscribe.php" ,
@@ -41,7 +44,9 @@
 	  );
 	  emailElement.value = "Thank You!";
 	  emailElement.disabled = true;
-	  emailSubmitBtn.disabled = true;
+	  emailSubmitBtn.style.display = "none";
+	  emailElement.style.border = "0px solid #999";
+	  emailElement.style.background = "#E6E7E9";
     };
    };
 </script>
@@ -63,17 +68,17 @@
         <div class="clearfix"></div>
         
         <div id="left_column">
-        	<img src="../images/screenshots/mobile_1.jpg" width="600" height="427" border="0">
+        	<img src="../images/screenshots/mobile_1.jpg" width="600" height="427" style="border: none;" alt="Mobile Screen Shot number 1.">
         </div>
         
         <div id="sidebar">
-        	<input type="checkbox" id="email_optin" name="email_optin" checked="true">Keep me up to date with Songbird news and software updates.</input>
-            <input type="text" name="email_addr" id="email_addr"  value="Email Address" size="24" onFocus="this.value=''" style="padding:10px; color:#999999; font-size:15px; font-family:Helvetica, Arial, sans-serif; border:1px solid #999999; width:288px; margin-top:20px;">
+        	<input type="checkbox" id="email_optin" name="email_optin" checked="checked">Keep me up to date with Songbird news and software updates.
+            <input type="text" name="email_addr" id="email_addr"  value="Email Address" size="24" onFocus="this.value=''" ><a href="javascript:void(0);" id="email_submit_btn" onClick="captureEmail();" ></a>
             <a href="http://market.android.com/details?id=com.songbirdnest.mediaplayer" id="download" target="_blank" ></a>
             
             <div id="iphone">
-            	<img src="../images/icon_iphone.png" border="0" width="23" height="44" align="left" style="margin:0 10px 0 0;"><strong>IPHONE COMING SOON</strong><br>submit your email to be the first to know about availability.<br>
-                <input type="text" name="iPhone_email_addr" id="iPhone_email_addr"  value="Email Address" size="24" onFocus="this.value=''" style="padding:10px; color:#999999; font-size:15px; font-family:Helvetica, Arial, sans-serif; margin-top:20px; background:url(../images/bg_emailsubmit.png) no-repeat; width:193px; height:17px; border:none"><input type="button" id="email_submit_btn" onClick="captureiPhoneEmail();" id="iPhone_email_addr" name="iPhone_email_addr" style="float:right; background:url(../images/bg_emailsubmit.png) no-repeat -213px 0; width:37px; height:37px; border:none; margin:20px 0 0 0;" >
+            	<img src="../images/icon_iphone.png" width="23" height="44" style="float: left; margin:0 10px 0 0; border: none;" alt="iPhone icon."><strong>IPHONE COMING SOON</strong><br>submit your email to be the first to know about availability.<br>
+                <input type="text" name="iPhone_email_addr" id="iPhone_email_addr"  value="Email Address" size="24" onFocus="this.value=''"><a href="javascript:void(0);" id="iPhone_email_submit_btn" onClick="captureiPhoneEmail();"></a>
             </div>
         </div>
         
@@ -81,17 +86,17 @@
         
         <div class="three_column" style="margin:40px 10px 0 0;">
         	<h1>Elegant Music Player</h1>
-            <img src="../images/screenshots/mobile_2.jpg" width="310" height="170px" style="margin:10px 0" border="0">
+            <img src="../images/screenshots/mobile_2.jpg" width="310" height="170" style="margin:10px 0 0 0; border: none;"  alt="Mobile Screenshot number 2.">
             Clean, sleek design. Browse, sort and play your music the way you want. Create, edit and sort playlists. Search through all your music. Matching homescreen and lockscreen widgets with album art. 
         </div>
         <div class="three_column" style="margin:40px 10px 0 0;">
         	<h1>Discover New Music</h1>
-            <img src="../images/screenshots/mobile_3.jpg" width="310" height="170px" style="margin:10px 0" border="0">
+            <img src="../images/screenshots/mobile_3.jpg" width="310" height="170" style="margin:10px 0 0 0; border: none;" alt="Mobile Screenshot number 3.">
             See which artists your friends are into. “Like” any track, follow any artist.  Share with friends quickly and easily – all with one click, in the same app! 
         </div>
         <div class="three_column" style="margin:40px 0px 0 0;">
         	<h1>Unique to You</h1>
-            <img src="../images/screenshots/mobile_4.jpg" width="310" height="170px" style="margin:10px 0" border="0">
+            <img src="../images/screenshots/mobile_4.jpg" width="310" height="170" style="margin:10px 0 0 0; border: none;"  alt="Mobile screenshot number 4.">
             Follow artists to automatically receive the latest updates including songs, videos, posts and more. All delivered to you to save you time and effort from searching.
         </div>
         
