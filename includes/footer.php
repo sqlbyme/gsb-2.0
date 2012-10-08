600fe516a2ddf8dbb1853f327f4b7b76bec36728
Mike Edwards <mike@songbirdnest.com>
2012-10-08 16:39:23 -0700

    Put the footer back per Neumann.

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<div id="lh_block" style="display:block; padding-left: 100px;"><div id="footer">
+<div id="footer">
   <div id="footer-content">
     <p>2012 &copy; All Rights Reserved &nbsp;&nbsp; | &nbsp;</p>
       <ul>
@@ -14,5 +14,5 @@
     <a class="facebook" href="http://www.facebook.com/Songbird" target="_blank" onClick="recordOutboundLink(this, 'Facebook Clicked', 'Link Location: Page Footer');return false;"></a><a class="twitter" href="https://twitter.com/#!/songbird" target="_blank" onClick="recordOutboundLink(this, 'Twitter Clicked', 'Link Location: Page Footer'); return false;"></a>
   </div><!--footer-content -->
   <div class="clearfix"></div>
-</div></div><!--footer -->
+</div><!--footer -->
 <script src="http://www.google.com/jsapi?key=ABQIAAAAb7YqigJmzMbctdfmEybjLhQw0f8hbslFnOgjefsaxzCjFKJ7khR5ileW-NerL3dofLA_jLbLLzCRkw" type="text/javascript"></script>
