1e6cb3333c8c414f2daad72fa700c3c142b9b823
Mike Edwards <mike@songbirdnest.com>
2013-05-02 16:55:12 -0700

    Working on the initial layout of philips mobile landing page per: https://www.pivotaltracker.com/story/show/49091825 .

--- philips/mobile/index.php
+++ philips/mobile/index.php
@@ -1,3 +1,4 @@
+<?php include('../../includes/download.php'); ?>
 <!DOCTYPE html>
 <!--[if IE 6]>
 <html id="ie6" dir="ltr" lang="en-US">
@@ -13,14 +14,86 @@
 <!--<![endif]-->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="apple-itunes-app" content="app-id=561535312">
-<title>Songbird Powered by Philips</title>
-<link rel="stylesheet" href="/css/style.css" type="text/css" media="all" />
-<meta name="viewport" content="width=1000">
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<title>Songbird</title>
+<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1"/>
+<style>
+body {
+  font-size: 62.5%;
+}
+body.sub {
+  background-image: none;
+  border: solid;
+  border-color: green;
+}
+#container {
+  position: relative;
+  margin: 0.313em 0 0 0.938em;
+  width: auto;
+  border: solid;
+  border-color: red;
+  background: url('../../images/mobile-logo-header.png') no-repeat;
+  background-size: 100%;
+}
+#header {
+  position: relative;
+  margin-bottom: 0.625em;
+  height: 4.063em;
+  border: solid;
+  
+}
+#sub-header {
+  position: relative;
+ 
+}
+#copy-container {
+  position: absolute;
+  top: 0.063em;
+  left: 0.063em;
+  width: 30.000em;
+}
+h1 {
+  font-size: 1.250em;
+}
+@media screen and (max-width: 30.00em) {
+
+	body.sub {
+	  border: none;
+	}
+	
+	#container {
+	  border: none;
+	}
+	
+	#header {
+	  border: none;
+	}
+
+}
+</style>
 </head>
 
-<body id="index" onload="">
-  Mobile Page
+<body class="sub">
+  <div id="container">
+    <div id="header">
+      <!--<img src="../../images/mobile-logo-header.png">-->
+    </div>
+    <div id="sub-header">
+      <div id="copy-container">
+        <h1>(re)discover music&#0153; with Songbird.</h1>
+        <p>Your own personalized music experience with photos and videos from the artists you love most.  Discover new music with YouTube playlists hand picked by the Songbird team.  Stream your favorite music to your wireless speakers and televisions.</p>
+      </div>
+    </div>
+  <div id="content" style="margin-bottom: 300px;">
+    
+        
+
+    </div><!--content -->
+<div class="clearfix"></div>
+<div class="clearfix"></div>
+</div><!--container -->
+
+<!-- include Google Analytics Tracking Code -->
+<?php include('../../includes/ga.php'); ?>
+<!-- End GA Include -->
 </body>
 </html>
