1cb0c473667560ceca1bbf5f6203d8e543e52085
Mike Edwards <mike@songbirdnest.com>
2013-05-08 17:02:32 -0700

    Working on layout fix.

--- philips/mobile/index.php
+++ philips/mobile/index.php
@@ -26,11 +26,14 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Songbird</title>
-<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1, maximum-scale=1"/>
+<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 <style>
 
-@media screen, handheld and (max-width: 30.00em) {
-  
+@media screen and (max-width: 20.00em) and (orientation: portrait) {
+ body {
+   font-family: Helvetica, Arial, "Times New Roman";
+   font-size: 62.5%;
+ } 
  #container {
     position: relative;
     margin: 0.313em auto;
@@ -158,6 +161,138 @@
     color:#e6317e;
   }
 }
+@media screen and (orientation: landscape) {
+  body {
+    font-family: Helvetica, Arial, "Times New Roman";
+    font-size: 87.5%;
+  } 
+  #container {
+     position: relative;
+     margin: 0.313em auto;
+     width: 30.000em;
+     height: 6.500em;
+     background: url('../../images/mobile-logo-header.png') no-repeat;
+     background-size: 100%;
+   }
+   #header {
+     position: absolute;
+     top: 3.033em;
+     left: 0.063em;
+     width: 30.000em;
+   }
+   h1 {
+     font-size: 1.250em;
+     color: #e6317e;
+   }
+ 	#content {
+ 	  position: relative;
+ 	  height: 80em;
+ 	  width: auto;
+ 	  top: 13.000em;
+ 	}
+ 	#iPhone-img {
+ 	  position: absolute;
+ 	  background: url('../../images/ios-product-shot.png') no-repeat;
+ 	  background-size: 100%;
+ 	  top: 1.000em;
+ 	  width: 14.575em;
+ 	  height: 26.000em;
+ 	}
+   #Android-img {
+     position: absolute;
+     background: url('../../images/android-product-shot.png') no-repeat;
+     background-size: 95%;
+     left: 15.000em;
+     width: 14.575em;
+     top: 1.225em;
+     height: 28.000em;
+   }
+   #appstore-badge {
+     position: absolute;
+     background: url('../../images/appstore-badge.png') no-repeat;
+     background-size: 95%;
+     top: 27.500em;
+     width: 14.575em;
+     height: 5.000em;
+   }
+   #googleplay-badge {
+     position: absolute;
+     background: url('../../images/googleplay-badge.png') no-repeat;
+     background-size: 95%;
+     left: 15.000em;
+     width: 14.575em;
+     top: 27.500em;
+     height: 5.000em;
+   }
+   #connect_copy {
+     position: absolute;
+     top: 33.000em;
+     width: 30.000em;
+   }
+   #connect_img {
+     position: absolute;
+     background: url('../../images/connect.png') no-repeat;
+     background-size: 87%;
+     top: 40.000em;
+     left: 1.500em;
+     width: 30.000em;
+     height: 16.450em;
+   }
+   #play_copy {
+     position: absolute;
+     top: 55.575em;
+     width: 30.000em;
+   }
+   #play_img {
+     position: absolute;
+     background: url('../../images/play.png') no-repeat;
+     background-size: 87%;
+     top: 62.000em;
+     left: 1.500em;
+     width: 30.000em;
+     height: 16.450em;
+   }
+   #like_copy {
+     position: absolute;
+     top: 78.000em;
+     width: 30.000em;
+   }
+   #like_img {
+     position: absolute;
+     background: url('../../images/like.png') no-repeat;
+     background-size: 87%;
+     top: 85.575em;
+     left: 1.500em;
+     width: 30.000em;
+     height: 16.450em;
+   }
+   #appstore-badge_footer {
+     position: absolute;
+     background: url('../../images/appstore-badge.png') no-repeat;
+     background-size: 95%;
+     top: 104.500em;
+     width: 14.575em;
+     height: 5.000em;
+   }
+   #googleplay-badge_footer {
+     position: absolute;
+     background: url('../../images/googleplay-badge.png') no-repeat;
+     background-size: 95%;
+     left: 15.000em;
+     width: 14.575em;
+     top: 104.500em;
+     height: 5.000em;
+   }
+   #footer_copy{
+     position: absolute;
+     top: 110.000em;
+     width: 30.000em;
+   }
+   a#footer_link {
+     text-decoration: none;
+     color:#e6317e;
+   }
+}
 </style>
 
 </head>
@@ -167,7 +302,7 @@
     
       <div id="container">
         <div id="header">
-          <h1>(re)discover music&#0153; with Songbird.</h1>
+          <h1>(re)discover music&#0153; with Songbird</h1>
           <p>Your own personalized music experience with photos and videos from the artists you love most.  Discover new music with YouTube playlists hand picked by the Songbird team.
              Stream your favorite music to your wireless speakers and televisions.</p>
         </div>
